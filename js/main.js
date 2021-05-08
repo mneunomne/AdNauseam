f8882aa92402cce6002e389fcc438c743d7c14df
dhowe <dhowe@cims.nyu.edu>
2021-05-08 16:43:53 +0800

    fix to bowser issue https://github.com/dhowe/AdNauseam/issues/228#issuecomment-835204569

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
 
   var original;
 
-  var openInTab = function(url) {
-    window.open(url,'_blank');
+  var openInTab = function (url) {
+    window.open(url, '_blank');
     window.open(url);
   }
 
@@ -19,37 +19,40 @@ $(document).ready(function () {
   // show the correct default install link (see #295)
   $("#install a").addClass('passive');
 
-  var sel = $("#install" + browser.name);
-  if (!sel.length) sel = $("#installChrome");
-  sel.removeClass('passive');
+  if (typeof bowser !== 'undefined') { // not for press page 
+    var sel = $("#install" + bowser.name);
+    if (!sel.length) sel = $("#installChrome");
+    sel.removeClass('passive');
 
-  if (browser.name === 'Opera')
-    $("#installChrome").css('right', '0px');
+    if (bowser.name === 'Opera') {
+      $("#installChrome").css('right', '0px');
+    }
+  }
 
   var bannedDate = new Date("Jan 1, 2017 00:00:00").getTime();
 
   // Update the count every 1 second
-   setInterval(function count() {
+  setInterval(function count() {
 
-      var now = new Date().getTime();
-      var distance = now - bannedDate;
+    var now = new Date().getTime();
+    var distance = now - bannedDate;
 
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      if(hours < 10) hours = "0" + hours;
-      if(minutes < 10) minutes = "0" + minutes;
-      if(seconds < 10) seconds = "0" + seconds;
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
 
-      // Display the result in the element with id="demo"
-      $(".countdown .days").text(days);
-      $(".countdown .hours").text(hours);
-      $(".countdown .mins").text(minutes);
-      $(".countdown .secs").text(seconds);
+    // Display the result in the element with id="demo"
+    $(".countdown .days").text(days);
+    $(".countdown .hours").text(hours);
+    $(".countdown .mins").text(minutes);
+    $(".countdown .secs").text(seconds);
 
-      return count;
+    return count;
 
   }(), 1000);
 
