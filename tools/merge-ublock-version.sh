4905a4cf4362eb96d63678088a21e8c70052ef4c
Alberto Harres <alberto.harres@gmail.com>
2022-09-26 01:56:23 +0200

    automating merging

--- tools/merge-ublock-version.sh
+++ tools/merge-ublock-version.sh
@@ -2,13 +2,14 @@
 
 VERSION=$1
 
-echo "Merging to uBlock $VERSION"
 
 if [[ -z $(git status -s) ]]
 then
-  echo "STAGED!!!"
+  echo "Merging to uBlock $VERSION"
+  git fetch --all --tags --prune
+  git checkout tags/$VERSION 
 else
-  echo "TEST!!!"
+  echo "There are uncommited changes, make sure you commit them before starting your merge"
 fi
 
 
