ac116ab014e71b31fbd2abaacc252745cd77a70d
Alberto Harres <alberto.harres@gmail.com>
2022-09-29 18:37:51 +0200

    adjust merge script

--- tools/merge-ublock-version.sh
+++ tools/merge-ublock-version.sh
@@ -4,23 +4,18 @@ echo "This is an automated merge script, only use it if you are aware how the us
 read -p "Are you sure you want to continue?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    # do dangerous stuff
+then  
+  VERSION=$1
+  if [[ -z $(git status -s) ]]
+  then
+    echo "Merging to uBlock $VERSION"
+    git fetch --all --tags --prune
+    git checkout tags/$VERSION
+    git checkout -b upstream$VERSION
+    git checkout master
+    git checkout -b merge$VERSION
+    git merge upstream$VERSION
+  else
+    echo "There are uncommited changes, make sure you commit them before starting your merge"
+  fi
 fi
-
-VERSION=$1
-
-if [[ -z $(git status -s) ]]
-then
-  echo "Merging to uBlock $VERSION"
-  git fetch --all --tags --prune
-  git checkout tags/$VERSION
-  git checkout -b upstream$VERSION
-  git checkout master
-  git checkout -b merge$VERSION
-  git merge upstream$VERSION
-else
-  echo "There are uncommited changes, make sure you commit them before starting your merge"
-fi
-
-
