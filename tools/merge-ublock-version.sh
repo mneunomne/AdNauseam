3391b72a21f3021a212d5431a21a14a5ed902f83
Alberto Harres <alberto.harres@gmail.com>
2022-09-29 18:35:29 +0200

    add 'are you sure' command on merge script

--- tools/merge-ublock-version.sh
+++ tools/merge-ublock-version.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-VERSION=$1
+echo "This is an automated merge script, only use it if you are aware how the usual uBlock -> AdNauseam mrege workflow works."
+read -p "Are you sure you want to continue?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+fi
 
+VERSION=$1
 
 if [[ -z $(git status -s) ]]
 then
