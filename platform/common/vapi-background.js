ab57d40c005bdac7d1f2dc9de650e5195a14dcd0
Alberto Harres <alberto.harres@gmail.com>
2022-10-24 18:30:35 +0200

    handle ports exception

--- platform/common/vapi-background.js
+++ platform/common/vapi-background.js
@@ -853,6 +853,12 @@ vAPI.messaging = {
     },
 
     onPortDisconnect: function(port) {
+        /* Adn */
+        if (port == null) {
+            if (vAPI.prefs.logEvents) console.error("onPortDisconnect: port is null")
+            return;
+        }
+        /* end of Adn */
         this.ports.delete(port.name);
     },
 
