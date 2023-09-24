4da4792a465c8983a3f7c2e47b64406f93e3a7d6
Alberto Harres <alberto.harres@gmail.com>
2023-09-24 22:24:55 +0100

    update merge ublock script

--- tools/merge-ublock-version.sh
+++ tools/merge-ublock-version.sh
@@ -8,16 +8,16 @@ then
   VERSION=$1
   BRANCH=${2:-master}
   echo "Merging to AdNauseam $VERSION on branch $BRANCH"
- # if [[ -z $(git status -s) ]]
- # then
- #   echo "Merging to uBlock $VERSION"
- #   git fetch --all --tags --prune
- #   git checkout tags/$VERSION
- #   git checkout -b upstream$VERSION
+  if [[ -z $(git status -s) ]]
+  then
+    echo "Merging to uBlock $VERSION"
+    git fetch --all --tags --prune
+    git checkout tags/$VERSION
+    git checkout -b upstream$VERSION
   git checkout "$BRANCH"
- #   git checkout -b merge$VERSION
- #   git merge upstream$VERSION
- # else
- #   echo "There are uncommited changes, make sure you commit them before starting your merge"
- # fi
+    git checkout -b merge$VERSION
+    git merge upstream$VERSION
+  else
+    echo "There are uncommited changes, make sure you commit them before starting your merge"
+  fi
 fi
