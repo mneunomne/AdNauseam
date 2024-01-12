04ff9b68e0998c4c4997a5152e169aeeeb539eae
Alberto Harres <alberto.harres@gmail.com>
2024-01-12 01:23:49 +0100

    noGenericCosmeticFiltering switch

--- src/js/messaging.js
+++ src/js/messaging.js
@@ -722,7 +722,7 @@ const retrieveContentScriptParameters = async function(sender, request) {
     const noSpecificCosmeticFiltering =
         pageStore.shouldApplySpecificCosmeticFilters(frameId) === false;
     // ADN https://github.com/dhowe/AdNauseam/issues/2038
-    const noGenericCosmeticFiltering = false; //pageStore.shouldApplyGenericCosmeticFilters(frameId) === false;
+    const noGenericCosmeticFiltering = pageStore.shouldApplyGenericCosmeticFilters(frameId) === false;
 
     const response = {
         collapseBlocked: µb.userSettings.collapseBlocked,
